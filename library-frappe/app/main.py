8669a98df7c933a4e6038262a0042c842d3b3db0
Savio Dias <diassavio629@gmail.com>
2025-01-15 22:13:27 +0530

    updated main

--- library-frappe/app/main.py
+++ library-frappe/app/main.py
@@ -5,7 +5,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], 
+    allow_origins=["https://frappe-test.netlify.app", "http://localhost:3000"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
